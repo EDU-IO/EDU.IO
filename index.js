1e6c0208c4b998a3c737f6eb6c1d71457018f279
Hugo Zhan <hugozhan0802@gmail.com>
2021-10-24 07:17:10 -0700

    connect cockroach

--- index.js
+++ index.js
@@ -45,4 +45,76 @@ const addFile = async (fileName, filePath) => {
     return fileHash
 }
 
+// Connect to CockroachDB through Sequelize.
+
+
+// Create the "accounts" table.
+function insertion() {
+    const Sequelize = require("sequelize-cockroachdb");
+  // For secure connection:
+    const fs = require(['fs']);
+    var sequelize = new Sequelize({
+      dialect: "postgres",
+      username: "andrewzhang76",
+      password: "LTqvU1pow8hYE1mp",
+      host: "free-tier.gcp-us-central1.cockroachlabs.cloud",
+      port: 26257,
+      database: "EDU-IO.defaultdb",
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: false,
+          // For secure connection:
+          ca: fs.readFileSync('certs/ca.crt')
+                   .toString()
+        },
+      },
+      logging: false,
+    });
+    
+    // Define the Account model for the "accounts" table.
+    const Account = sequelize.define("accounts", {
+      id: {
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      balance: {
+        type: Sequelize.INTEGER,
+      },
+      password:{
+        type: Sequelize.STRING,
+      }
+      
+    });
+    Account.sync({
+      force: true,
+    })
+      .then(function () {
+        // Insert two rows into the "accounts" table.
+        return Account.bulkCreate([
+          {
+            id: "andrewzhang76@berkeley.edu",
+            balance: 1000,
+            password:"zjm010706",
+          },
+          {
+            id: "jerryhu@berkeley.edu",
+            balance: 250,
+            password:"huyujie1207",
+          },
+        ]);
+      })
+      .then(function (accounts) {
+        // Print out the balances.
+        accounts.forEach(function (account) {
+          console.log(account.id + " " + account.balance);
+        });
+        process.exit(0);
+      })
+      .catch(function (err) {
+        console.error("error: " + err.message);
+        process.exit(1);
+      });
+  }
+  
+
 
